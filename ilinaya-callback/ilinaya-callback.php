f2efd524d705d84495b3ab45ca55856052be8212
ilinsky <alex.ilinsky@gmail.com>
2020-05-14 17:06:23 -0700

    Initial commit

---
+++ ilinaya-callback/ilinaya-callback.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Ilinaya CallBack
+Description: Get Calls from your website to your call center
+Author: ilinaya
+*/
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
