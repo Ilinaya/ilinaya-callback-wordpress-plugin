ffeaaee73905da7f3afcbc82abcd38cd28a35d7c
ilinsky <alex.ilinsky@gmail.com>
2020-05-20 21:42:43 -0700

    WordPress Plugin

--- ilinaya-callback/ilinaya-callback.php
+++ ilinaya-callback/ilinaya-callback.php
@@ -4,4 +4,6 @@ Plugin Name: Ilinaya CallBack
 Description: Get Calls from your website to your call center
 Author: ilinaya
 */
-require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+require_once plugin_dir_path( __FILE__ ).'class.php';
+
+$settings = new IlinayaWidget( "Callback Ilinaya", "callback_ilinaya", __FILE__ );
